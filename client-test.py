ae1e9eead359713734289ca34989bde1d8f6f15d
Wei Ouyang <oeway007@gmail.com>
2022-08-10 15:19:03 -0700

    Add leap-win-server functions

--- client-test.py
+++ client-test.py
@@ -2,6 +2,17 @@ from __future__ import print_function
 import Pyro4
 
 uri = input("Enter the uri of the warehouse: ").strip()
-warehouse = Pyro4.Proxy(uri)
+leap_win_server = Pyro4.Proxy(uri)
 
-print(warehouse.list_contents())
+def test_server():
+    data = leap_win_server.read_spectrometer()
+    assert len(data) == 2 and len(data[0]) == 1024 and len(data[1]) == 1024
+    
+    ret = leap_win_server.set_gating_parameters(10, 100)
+    assert ret == True
+    
+    
+    print("tests passed")
+    
+if __name__=="__main__":
+    test_server()
---
+++ start-leap-win-server.py
@@ -0,0 +1,56 @@
+from __future__ import print_function
+import Pyro4
+import numpy as np
+
+@Pyro4.expose
+@Pyro4.behavior(instance_mode="single")
+class LeapWinServer(object):
+    def __init__(self):
+        self.contents = ["chair", "bike", "flashlight", "laptop", "couch"]
+
+    def set_spectrometer_parameters(self, config):
+        print("set_spectrometer_parameters")
+        return True
+
+    def read_spectrometer(self):
+        # generate a random array with 2x 1024 elements in float between 0 and 65535.0
+        data = np.random.randint(0, 65535, size=(2, 1024))
+        print("read_spectrometer")
+        return data.tolist()
+
+    def turn_on_laser(self):
+        print("Laser turned on.")
+        return True
+
+    def set_laser_wavelength(self, wavelength):
+        print("Laser wavelength set to {0}.".format(wavelength))
+        return True
+
+    def turn_off_laser(self):
+        print("Laser turned off.")
+        return True
+    
+    def start_gating(self):
+        print("gating started.")
+        return True
+    
+    def set_gating_parameters(self, gate_width, gate_delay):
+        print("gating parameters set to {0} and {1}.".format(gate_width, gate_delay))
+        return True
+    
+    def read_iv_curve_live(self, size=1024):
+        print("iv curve read.")
+        return np.random.randint(0, 65535, size=(2, size))
+
+def main():
+    Pyro4.Daemon.serveSimple(
+        {
+            LeapWinServer: "leap_win_server"
+        },
+        host="0.0.0.0",
+        port=9090,\
+        ns=False
+    )
+
+if __name__=="__main__":
+    main()
